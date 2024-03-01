f54ef3501fe0da5727bdf6e1373e45c92adf9f31
Shellahariza <150937297+Shellahariza@users.noreply.github.com>
2024-03-01 21:40:20 +0700

    cobacobaae by shel

--- index.php
+++ index.php
@@ -58,9 +58,9 @@ require 'ceklogin.php';
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-body">Informasi lainnya</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <a class="small text-white stretched-link" href="#">Tampilkan lainnnya</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
