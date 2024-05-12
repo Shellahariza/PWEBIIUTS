35d2ac5df7aa4a1457ac83c543e8618e6e56787e
aleyananda <152393386+aleyananda@users.noreply.github.com>
2024-05-12 23:03:10 +0700

    commit by ale

--- masuk.php
+++ masuk.php
@@ -41,6 +41,10 @@ require 'ceklogin.php';
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Barang Masuk
                             </a>
+                            <a class="nav-link" href="pelanggan.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Kelola Pelanggan
+                            </a>
                             <a class="nav-link" href="logout.php">
                                 Logout
                             </a>
